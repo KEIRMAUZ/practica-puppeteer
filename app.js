ed2bcc8d86722877dd079765e5860f0f11f6daeb
KEIRMAUZ <2022023@utsh.edu.mx>
2025-05-29 20:46:04 -0500

    [ADD] Se añadio la funcion de tomar captura

--- app.js
+++ app.js
@@ -13,4 +13,24 @@ async function abrirPaginaWeb() {
     console.log('::Termine')
 }
 
-abrirPaginaWeb()
+//abrirPaginaWeb()
+
+async function capturarScreen() {
+    console.log('::Inicio')
+    const navegador = await puppeteer.launch({
+        headless:false,
+        slowMo:800
+    });
+
+    const pagina = await navegador.newPage();
+    await pagina.goto('https://utsh.edu.mx/');
+    await pagina.screenshot({
+        path:"./screenshoots/ejemplo.png",
+        fullPage:true
+    })
+    navegador.close();
+
+    console.log('::Termine')
+}
+
+capturarScreen()
