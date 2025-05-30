289487c0c09e313b5ed52fa7b31ab2d50749fcd7
KEIRMAUZ <2022023@utsh.edu.mx>
2025-05-30 11:27:01 -0600

    [ADD] Se agrego la funcion de navegar entre paginas

--- app.js
+++ app.js
@@ -26,11 +26,36 @@ async function capturarScreen() {
     await pagina.goto('https://utsh.edu.mx/');
     await pagina.screenshot({
         path:"./screenshoots/ejemplo.png",
-        fullPage:true
+        fullPage:false
     })
     navegador.close();
 
     console.log('::Termine')
 }
 
-capturarScreen()
+//capturarScreen()
+
+async function navegarEntrePaginas(){
+    console.log('::Inicio')
+    const navegador = await puppeteer.launch({
+        headless:false,
+        slowMo:800
+    });
+
+    const pagina = await navegador.newPage();
+    await pagina.goto('https://example.com/');
+
+    await pagina.click('div>p>a[href="https://www.iana.org/domains/example"]');
+
+    await new Promise ((resolve)=>setTimeout(resolve,300))
+
+    await pagina.screenshot({
+        path:'./screenshoots/Pagina-visitada-Autom.png',
+        fullPage:true,
+    });
+    navegador.close();
+
+    console.log('::Termine')
+}
+
+navegarEntrePaginas()
